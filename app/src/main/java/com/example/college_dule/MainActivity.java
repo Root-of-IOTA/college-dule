41eb1046c3aac31e3c0ab87a4525497877a1964e
kamalraj D <kamalrajdhanakodi@gmail.com>
2021-03-26 00:44:13 +0530

    backend corrected

--- app/src/main/java/com/example/college_dule/MainActivity.java
+++ app/src/main/java/com/example/college_dule/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.college_dule;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin = (Button) findViewById(R.id.loginbutton);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), leaderpage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/college_dule/leaderpage.java
+++ app/src/main/java/com/example/college_dule/leaderpage.java
@@ -1,21 +1,15 @@
 package com.example.college_dule;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.wearable.activity.WearableActivity;
 import android.widget.TextView;
 
-public class leaderpage extends WearableActivity {
-
-    private TextView mTextView;
+public class leaderpage extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderpage);
-
-        mTextView = (TextView) findViewById(R.id.text);
-
-        // Enables Always-on
-        setAmbientEnabled();
+        System.out.println("entered");
     }
 }
