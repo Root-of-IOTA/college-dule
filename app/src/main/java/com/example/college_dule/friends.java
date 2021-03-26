38b387925de6f01d36bee7a13af049f9ae312be7
DHANUSH-6 <dhanushpowerk6@gmail.com>
2021-03-26 14:34:36 +0530

    connected the planner

--- app/src/main/java/com/example/college_dule/friends.java
+++ app/src/main/java/com/example/college_dule/friends.java
@@ -2,13 +2,22 @@ package com.example.college_dule;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class friends extends AppCompatActivity {
-
+    Button btnAddEvent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+
+        btnAddEvent = (Button) findViewById(R.id.add_plan_friends);
+        btnAddEvent.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), plan_detail.class);
+            startActivity(intent);
+        }
+        );
     }
 }
